350d46f1094ed3f10268f06a3ec734dea88464e3
Cats-Eye <ef_beee_i@yahoo.co.jp>
2017-09-27 20:41:38 +0900

    右端、上端のバグ潰し完了。健全に動くようになった labelとfrontierの関数化

--- gridgraph.py
+++ gridgraph.py
@@ -1,7 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import copy
-def org_grid_2d_graph(m,n,periodic=False,create_using=None): #重み付き二次元格子グラフの生成関数
+
+def org_grid_2d_graph(m, n, periodic=False, create_using=None): #重み付き二次元格子グラフの生成関数
     G=nx.DiGraph()
     G.name="grid_2d_graph"
     rows=range(m)
@@ -11,7 +12,23 @@ def org_grid_2d_graph(m,n,periodic=False,create_using=None): #重み付き二次
     G.add_weighted_edges_from( [(i,j-1),(i,j),0] for i in rows for j in columns if j>0 )
     return G
 
-input =2  #考えたいgridの一辺の長さ
+def label_generate(temp_edge_g, input_g):#ラベル生成関数
+    label_g = 0
+    for i in range(0,input_g+1):#フロンティアについてラベル付け
+        label_g = label_g + (2**i)*temp_edge_g[i]#ただし逆順で計算しているので注意
+        # print(i)
+        # print(label,"=",label,"+","2^",i,"*",temp_edge[i])
+    return(label_g)
+
+def newfrontier_generate(label_g, temp_edge_g, newfrontier_g, frontier_g):#フロンティア生成関数
+    if label_g in newfrontier_g:
+        newfrontier_g[label_g]['count'] = newfrontier_g[label_g]['count'] + frontier_g[key]['count']
+    else:
+        newfrontier_g.setdefault(label_g, {})['edge'] = copy.deepcopy(temp_edge_g)#フロンティアの辺の向き
+        newfrontier.setdefault(label_g, {})['count'] = frontier_g[key]['count']#場合の数
+    return(0)
+
+input =3  #考えたいgridの一辺の長さ
 n = input + 2 #それに２を足す
 G = org_grid_2d_graph(n, n)
 
@@ -33,10 +50,6 @@ for ((s,t),(p,q)) in G.edges_iter(): #テスト
     if s == 0 or p == n-1:
         G.edge[(s,t)][(p,q)]['weight'] = 0
 
-# for s in range(n):
-#     G.edge[(0,s)][(1,s)]['weight'] = 0
-#     G.edge[(n-1,s)][(n,s)]['weight'] = 0
-
 # G.edge[(1,0)][(1,1)]['weight'] = 0 #解が２になる小池さんグリッド
 # G.edge[(2,0)][(2,1)]['weight'] = 0
 # G.edge[(2,1)][(3,1)]['weight'] = 0
@@ -48,10 +61,7 @@ label=0
 temp_edge.append(G.edge[(0,1)][(1,1)]['weight'])#横向き辺を最初に追加
 for i in range(1,input+1):#最下段のフロンティアを入力
     temp_edge.append(G.edge[(i,0)][(i,1)]['weight'])
-for i in range(0,input+1):#フロンティアについてラベル付け
-    label = label + (2**i)*temp_edge[i]#ただし逆順で計算しているので注意
-    # print(i)
-    # print(label,"=",label,"+","2^",i,"*",temp_edge[i])
+label=label_generate(temp_edge,input)
 
 #energysum=0
 # energy={1:[1 1 1 1],
@@ -73,8 +83,9 @@ for j in range(1,input+1):#1~n-2まですべての行
             temp_edge=frontier[key]['edge']#１つのフロンティアについてedgeを取り出す
             weightsum = temp_edge[0] + temp_edge[i]#まず処理済の左と下のweighttsumを求める
             print((i,j))
-            print(frontier,"temp_edge",temp_edge,"について計算スタート")
-            print("weightsumは",weightsum)
+            print(frontier)
+            print(temp_edge,"について計算開始")
+
             if i == input and j == input: #右上端
                 print("一番右上端")
                 if G.edge[(i,j)][(i,j+1)]['weight'] == 0:#上は入る矢印
@@ -84,120 +95,74 @@ for j in range(1,input+1):#1~n-2まですべての行
                 if weightsum == 2:#このパターンは有
                     temp_edge[i] = G.edge[(i,j)][(i,j+1)]['weight']
                     temp_edge[0] = G.edge[(i,j)][(i+1,j)]['weight']
-                    label=0
-                    for s in range(0,input+1):#ラベル生成
-                        label = label + (2**s)*temp_edge[s]
-                    if label in newfrontier:
-                        newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
-                    else:
-                        newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
-                        newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
+                    label=label_generate(temp_edge,input)
+                    newfrontier_generate(label, temp_edge, newfrontier, frontier)
+                    # if label in newfrontier:
+                    #     newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
+                    # else:
+                    #     newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
+                    #     newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
 
             elif i == input: #一番右端の行で右が処理済
                 print("一番右端")
                 if G.edge[(i,j)][(i+1,j)]['weight'] == 0: #右は入る矢印
-                    weightsum = weightsum + 1#入るのが0か3本なら不可
+                    weightsum = weightsum + 1
                 if weightsum == 1: #入るのが１本なら
                     temp_edge[i] = 0#上は入る矢印
                     temp_edge[0] = G.edge[(0,j+1)][(1,j+1)]['weight']
-                    label=0
-                    for s in range(0,input+1):#ラベル生成
-                        label = label + (2**s)*temp_edge[s]
-                    if label in newfrontier:
-                        newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
-                    else:
-                        newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
-                        newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
+                    label=label_generate(temp_edge,input)
+                    newfrontier_generate(label, temp_edge, newfrontier, frontier)
 
-                else: # weightsum == 2で入るのが2本なら
+                if weightsum == 2: #入るのが2本なら
                     temp_edge[i] = 1#上は出る矢印
                     temp_edge[0] = G.edge[(0,j+1)][(1,j+1)]['weight']
-                    label=0
-                    for s in range(0,input+1):#ラベル生成
-                        label = label + (2**s)*temp_edge[s]
-                    if label in newfrontier:
-                        newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
-                    else:
-                        newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
-                        newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
+                    label=label_generate(temp_edge,input)
+                    newfrontier_generate(label, temp_edge, newfrontier, frontier)
 
             elif j == input: #一番上端の行で上が処理済
                 print("一番上端")
                 if G.edge[(i,j)][(i,j+1)]['weight'] == 0: #上は入る矢印
-                    weightsum = weightsum + 1#入るのが0か3本なら不可
+                    weightsum = weightsum + 1
 
                 if weightsum == 1: #入るのが１本なら
                     temp_edge[i] = G.edge[(i,j)][(i,j+1)]['weight']
                     temp_edge[0] = 0
-                    label=0
-                    for s in range(0,input+1):#ラベル生成
-                        label = label + (2**s)*temp_edge[s]
-                    if label in newfrontier:
-                        newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
-                    else:
-                        newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
-                        newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
+                    label=label_generate(temp_edge,input)
+                    newfrontier_generate(label, temp_edge, newfrontier, frontier)
 
-                else: # weightsum == 2で入るのが2本なら
+                if weightsum == 2: #入るのが2本なら
                     temp_edge[i] = G.edge[(i,j)][(i,j+1)]['weight']
                     temp_edge[0] = 1#右は出る矢印
-                    label=0
-                    for s in range(0,input+1):#ラベル生成
-                        label = label + (2**s)*temp_edge[s]
-                    if label in newfrontier:
-                        newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
-                    else:
-                        newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
-                        newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
+                    label=label_generate(temp_edge,input)
+                    newfrontier_generate(label, temp_edge, newfrontier, frontier)
 
             else: #右も上も未処理
+                print("右も上も未処理")
                 if weightsum == 0: #出るのが2本なら
+                    print("出るのが２本")
                     temp_edge[i] = 0#上は入る矢印
                     temp_edge[0] = 0#右も入る矢印
-                    label=0
-                    for s in range(0,input+1):#ラベル生成
-                        label = label + (2**s)*temp_edge[s]
-                    if label in newfrontier:
-                        newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
-                    else:
-                        newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
-                        newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
+                    label=label_generate(temp_edge,input)
+                    newfrontier_generate(label, temp_edge, newfrontier, frontier)
 
                 elif weightsum == 1: #入るのが１本なら２パターン
+                    print("入るのが１本なら２パターン")
                     temp_edge[i] = 0 #上は入る矢印
                     temp_edge[0] = 1 #右は出る矢印
-                    label=0
-                    print("入るのが１本",temp_edge)
-                    for s in range(0,input+1):#ラベル生成
-                        label = label + (2**s)*temp_edge[s]
-                    if label in newfrontier:
-                        newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
-                    else:
-                        newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
-                        newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
+                    label=label_generate(temp_edge,input)
+                    newfrontier_generate(label, temp_edge, newfrontier, frontier)
 
                     temp_edge[i] = 1 #上は出る矢印
                     temp_edge[0] = 0 #右は入る矢印
-                    label=0
-                    for s in range(0,input+1):#ラベル生成
-                        label = label + (2**s)*temp_edge[s]
-                    if label in newfrontier:
-                        newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
-                    else:
-                        newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
-                        newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
+                    label=label_generate(temp_edge,input)
+                    newfrontier_generate(label, temp_edge, newfrontier, frontier)
 
                 else: #weightsum == 2で入るのが2本なら
+                    print("入るのが２本")
                     temp_edge[i] = 1 #上は出る矢印
                     temp_edge[0] = 1 #右も出る矢印
-                    label=0
-                    for s in range(0,input+1):#ラベル生成
-                        label = label + (2**s)*temp_edge[s]
-                    if label in newfrontier:
-                        newfrontier[label]['count'] = newfrontier[label]['count'] + frontier[key]['count']
-                    else:
-                        newfrontier.setdefault(label, {})['edge'] = copy.deepcopy(temp_edge)#フロンティアの辺の向き
-                        newfrontier.setdefault(label, {})['count'] = frontier[key]['count']#場合の数
+                    label=label_generate(temp_edge,input)
+                    newfrontier_generate(label, temp_edge, newfrontier, frontier)
 
         frontier = newfrontier
         newfrontier={}
