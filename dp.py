e4baf6c4ae002fa17fe0842bad151ba985aed833
Cats-Eye <ef_beee_i@yahoo.co.jp>
2017-12-08 16:24:40 +0900

    no message

--- dp.py
+++ dp.py
@@ -3,22 +3,16 @@ import time
 
 start = time.time()
 
-def label_generate(frontier_g):#ラベル生成関数
-    label_g = 0
-    for i in range(0,input+1):#フロンティアについてラベル付け
-        if frontier_g[i] == -1:
-            label_g = label_g + (3**i)*0#ただし逆順で計算しているので注意
-        elif frontier_g[i] == 0:
-            label_g = label_g + (3**i)*1
-        else:
-            label_g = label_g + (3**i)*2
-    return(label_g)
+input = 3 #考えたいgridの一辺の長さ
 
 def frontier_generate(label_g):#frontier生成関数
     frontier_g = [0]*(input+1)
     for i in range(0,input+1):
-        frontier_g[i] = label_g % 3 - 1 # [0, 1, 2]が[-1, 0, 1]に対応
-        label_g = label_g // 3
+        if label_g % 2 == 0: # [0, 1]が[-1, 1]に対応
+            frontier_g[i] = -1
+        else:
+            frontier_g[i] = 1
+        label_g = label_g // 2
     return(frontier_g)
 
 # 対象ノードの(左, 下)の状態からみて可能なパターン
@@ -47,7 +41,6 @@ energy_dic={0:1, #各配置におけるエネルギー
             4:5,
             5:6}
 
-input = 12 #考えたいgridの一辺の長さ
 nodesum = input**2 #node総数
 frontsum = 1
 
@@ -56,17 +49,59 @@ OldFronts = {} #各ノードにおける更新前front集合
 NewFronts = {} #各ノードにおける更新後front集合
 
 first_frontier = [0]*(input+1) #初期frontier
-label = label_generate(first_frontier)
-OldFronts.setdefault(0, {}).setdefault(label, 1)
+OldFronts.setdefault(0, {}).setdefault(0, 1)
 
-for node in range(nodesum, 0, -1): #各nodeについて
+for node in range(nodesum, nodesum-input, -1): #最下段nodeについて
     place = (nodesum - node + 1) % input #左から数えたnode位置
-    if place == 0: #右端は割り切れて0になるのでinputに書き換え
+    if place == 0:
         place = input
-
     for energysum in OldFronts:
         for label in OldFronts[energysum]:
             new_frontier = frontier_generate(label)
+            old_label = label #次に渡すラベル準備
+            if new_frontier[0] == 1:
+                old_label -= (2**0)*1
+            if new_frontier[place] == 1:
+                old_label -= (2**place)*1
+
+            if place == 1: #左端の場合
+                new_frontier[0] = 0 #左は未処理
+            new_frontier[place] = 0 #下は未処理
+            patterns = pattern_dic[(new_frontier[0], new_frontier[place])] #左と下の状態から
+
+            for i in patterns: #有りパターン
+                new_frontier[0] = direction_dic[i][0] #フロンティアを更新
+                new_frontier[place] = direction_dic[i][1]
+                new_label = old_label
+
+                if new_frontier[0] == 1: #ラベルを更新
+                    new_label += (2**0)*1
+                if new_frontier[place] == 1:
+                    new_label += (2**place)*1
+                new_energysum = energysum + energy_dic[i] #エネルギー合計を更新
+
+                if (new_energysum in NewFronts) and (new_label in NewFronts[new_energysum]): #同じ条件のものがあれば
+                    NewFronts[new_energysum][new_label] += OldFronts[energysum][label] #場合の数を追加
+                else: #同じ条件のものがなかった場合
+                    NewFronts.setdefault(new_energysum, {}).setdefault(new_label, OldFronts[energysum][label]) #新しくfrontを作る
+                    frontsum += 1
+    OldFronts = NewFronts
+    NewFronts = {}
+
+for node in range(nodesum-input, 0, -1): #各nodeについて
+    place = (nodesum - node + 1) % input #左から数えたnode位置
+    if place == 0: #右端は割り切れて0になるのでinputに書き換え
+        place = input
+    for energysum in OldFronts:
+        for label in OldFronts[energysum]:
+            new_frontier = frontier_generate(label)
+
+            old_label = label #次に渡すラベル準備
+            if new_frontier[0] == 1:
+                old_label -= (2**0)*1
+            if new_frontier[place] == 1:
+                old_label -= (2**place)*1
+
             if place == 1: #行が１つ上になって左端の場合
                 new_frontier[0] = 0 #左は未処理
             patterns = pattern_dic[(new_frontier[0], new_frontier[place])] #左と下の状態から
@@ -74,7 +109,11 @@ for node in range(nodesum, 0, -1): #各nodeについて
             for i in patterns: #有りパターン
                 new_frontier[0] = direction_dic[i][0] #フロンティアを更新
                 new_frontier[place] = direction_dic[i][1]
-                new_label = label_generate(new_frontier)
+                new_label = old_label
+                if new_frontier[0] == 1: #ラベルを更新
+                    new_label += (2**0)*1
+                if new_frontier[place] == 1:
+                    new_label += (2**place)*1
                 new_energysum = energysum + energy_dic[i] #エネルギー合計を更新
 
                 if (new_energysum in NewFronts) and (new_label in NewFronts[new_energysum]): #同じ条件のものがあれば
@@ -87,22 +126,19 @@ for node in range(nodesum, 0, -1): #各nodeについて
 
 Energy = {}
 sum = 0
-finalfrontsum = 0
-
 for energysum in OldFronts:
     Energy[energysum] = 0
     for label in OldFronts[energysum]:
         sum += OldFronts[energysum][label] #配置パターン総数を求める
         Energy[energysum] += OldFronts[energysum][label] #エネルギー総計毎にまとめる
-        finalfrontsum += 1
 
 elapsed_time = time.time() - start
 f = open('result.txt', 'w')
 f.write("input" + str(input) + "の配置総数は" + str(sum) + "\n")
 f.write("elapsed_time:{0}".format(elapsed_time) + "[sec]" + "\n")
-f.write("フロント総数" + str(frontsum) + "最終フロント総数は" + str(finalfrontsum) + "\n")
+f.write("フロント総数" + str(frontsum) +"\n")
 
 # for k, v in sorted(Energy.items()):
-    # f.write(str(k) + " " + str(v) + "\n")
+#     f.write(str(k) + " " + str(v) + "\n")
 
 f.close()
