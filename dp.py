a5be0c58235ea579b1a765f0936fafd76183d024
Cats-Eye <ef_beee_i@yahoo.co.jp>
2017-12-08 16:52:42 +0900

    label生成関数の組み込み

--- dp.py
+++ dp.py
@@ -3,7 +3,7 @@ import time
 
 start = time.time()
 
-input = 3 #考えたいgridの一辺の長さ
+input = 2 #考えたいgridの一辺の長さ
 
 def frontier_generate(label_g):#frontier生成関数
     frontier_g = [0]*(input+1)
@@ -51,13 +51,14 @@ NewFronts = {} #各ノードにおける更新後front集合
 first_frontier = [0]*(input+1) #初期frontier
 OldFronts.setdefault(0, {}).setdefault(0, 1)
 
-for node in range(nodesum, nodesum-input, -1): #最下段nodeについて
+for node in range(nodesum, 0, -1): #各nodeについて
     place = (nodesum - node + 1) % input #左から数えたnode位置
     if place == 0:
         place = input
     for energysum in OldFronts:
         for label in OldFronts[energysum]:
             new_frontier = frontier_generate(label)
+
             old_label = label #次に渡すラベル準備
             if new_frontier[0] == 1:
                 old_label -= (2**0)*1
@@ -66,7 +67,8 @@ for node in range(nodesum, nodesum-input, -1): #最下段nodeについて
 
             if place == 1: #左端の場合
                 new_frontier[0] = 0 #左は未処理
-            new_frontier[place] = 0 #下は未処理
+            if node in range(nodesum-input, nodesum+1): #最下段の場合
+                new_frontier[place] = 0 #下は未処理
             patterns = pattern_dic[(new_frontier[0], new_frontier[place])] #左と下の状態から
 
             for i in patterns: #有りパターン
@@ -74,42 +76,6 @@ for node in range(nodesum, nodesum-input, -1): #最下段nodeについて
                 new_frontier[place] = direction_dic[i][1]
                 new_label = old_label
 
-                if new_frontier[0] == 1: #ラベルを更新
-                    new_label += (2**0)*1
-                if new_frontier[place] == 1:
-                    new_label += (2**place)*1
-                new_energysum = energysum + energy_dic[i] #エネルギー合計を更新
-
-                if (new_energysum in NewFronts) and (new_label in NewFronts[new_energysum]): #同じ条件のものがあれば
-                    NewFronts[new_energysum][new_label] += OldFronts[energysum][label] #場合の数を追加
-                else: #同じ条件のものがなかった場合
-                    NewFronts.setdefault(new_energysum, {}).setdefault(new_label, OldFronts[energysum][label]) #新しくfrontを作る
-                    frontsum += 1
-    OldFronts = NewFronts
-    NewFronts = {}
-
-for node in range(nodesum-input, 0, -1): #各nodeについて
-    place = (nodesum - node + 1) % input #左から数えたnode位置
-    if place == 0: #右端は割り切れて0になるのでinputに書き換え
-        place = input
-    for energysum in OldFronts:
-        for label in OldFronts[energysum]:
-            new_frontier = frontier_generate(label)
-
-            old_label = label #次に渡すラベル準備
-            if new_frontier[0] == 1:
-                old_label -= (2**0)*1
-            if new_frontier[place] == 1:
-                old_label -= (2**place)*1
-
-            if place == 1: #行が１つ上になって左端の場合
-                new_frontier[0] = 0 #左は未処理
-            patterns = pattern_dic[(new_frontier[0], new_frontier[place])] #左と下の状態から
-
-            for i in patterns: #有りパターン
-                new_frontier[0] = direction_dic[i][0] #フロンティアを更新
-                new_frontier[place] = direction_dic[i][1]
-                new_label = old_label
                 if new_frontier[0] == 1: #ラベルを更新
                     new_label += (2**0)*1
                 if new_frontier[place] == 1:
