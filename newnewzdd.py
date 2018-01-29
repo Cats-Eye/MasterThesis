c6d89b4c0e4bdc522e709d12a919ad0b2547b21c
Cats-Eye <ef_beee_i@yahoo.co.jp>
2018-01-29 15:50:26 +0900

    no message

--- newnewzdd.py
+++ newnewzdd.py
@@ -7,11 +7,12 @@ import time
 
 start = time.time()
 
-input = 14 #考えたいgridの一辺の長さ
+input = 9 #考えたいgridの一辺の長さ
 
 class Node(object):
     # count = 0
     def __init__(self, l, fl, parents_pattern=''): #初期化　コンストラクタ
+        self.index = str((l,len(levelset[l]))) #(level,level内ラベリング)というnode固有インデックス
         self.flabel = fl #ビット化したfrontier
         self.child = [0]*6 #各パターンへの子ノードへの枝
         self.parents = [parents_pattern] #親への枝(親ノード,パターン)
@@ -32,8 +33,11 @@ class Node(object):
             else:
                 patterns = pattern_dic[(self.flabel & 1, (self.flabel >> place) & 1)]
 
-        # for i in patterns[0]: #無しパターン
-            # self.child[i] = falseend #o終端に関しては親への枝を書き足さない
+        for i in patterns[0]: #無しパターン
+            # print(i, "なし", end =' ')
+            # for parent in self.parents:
+                # parent[1].child　枝が伸びてるのが自分だけならparentsを消去
+            self.child[i] = falseend.index #o終端に関しては親への枝を書き足さない
 
         for i in patterns[1]: #有りパターン
             # Node.count += 1
@@ -44,19 +48,20 @@ class Node(object):
                 new_label += (1 << place)
 
             if l == 0: #最後の段
-                self.child[i] = trueend #1終端につなぐ
-                trueend.parents.append((self, i))
+                self.child[i] = trueend.index #1終端につなぐ
+                trueend.parents.append((self.index, i))
 
             else:
                 if new_label in frontierset: #同じfrontierのnodeがあれば
-                    same_node = frontierset[new_label]
+                    same_node = Nodes[frontierset[new_label]]
                     self.child[i] = frontierset[new_label] #そのnodeに子供の枝を追加
-                    same_node.parents.append((self, i)) #そのnodeの親への枝を追加
+                    same_node.parents.append((self.index, i)) #そのnodeの親への枝を追加
                 else: #同じfrontierをもつnodeがない
-                    new_node = Node(l, new_label, (self, i)) #新しくnodeを作る
-                    levelset[l].append(new_node)
-                    frontierset[new_label] = new_node
-                    self.child[i] = new_node
+                    new_node = Node(l, new_label, (self.index, i)) #新しくnodeを作る
+                    levelset[l].append(new_node.index)
+                    frontierset[new_label] = new_node.index
+                    Nodes[new_node.index] =  new_node
+                    self.child[i] = new_node.index
 
 # 対象ノードの(左, 下)の状態からみて可能なパターン
 # -1は方向未処理、0は負方向、1は正方向
@@ -92,18 +97,22 @@ musk = [] #ビット演算マスク用
 for i in range(input+1):
     musk.append(bit - (1 << i))
 
+Nodes = {} #indexをキーとしたNodeインスタンスのディクショナリ
 levelset = {} #各高さにおけるnode集合
 for level in range(nodesum, -2, -1): #-1~nodesumまで
     levelset[level]=[]
 
 root = Node(nodesum, 0) #根頂点
-levelset[nodesum].append(root)
+levelset[nodesum].append(root.index)
+Nodes[root.index] =  root
 
 falseend = Node(-1, 0) #0終端
-levelset[-1].append(falseend)
+levelset[-1].append(falseend.index)
+Nodes[falseend.index] =  falseend
 
 trueend = Node(0, 0) #1終端
-levelset[0].append(trueend)
+levelset[0].append(trueend.index)
+Nodes[trueend.index] =  trueend
 
 for level in range(nodesum, 0, -1): #1~nodesumまでの各レベルについて
     place = (nodesum - level + 1) % input #左から数えたnode位置
@@ -111,62 +120,56 @@ for level in range(nodesum, 0, -1): #1~nodesumまでの各レベルについて
     if place == 0: #右端は割り切れて0になるのでinputに書き換え
         place = input
 
-    for node in levelset[level]: #各レベルにあるnodeについて
+    for node_index in levelset[level]: #各レベルにあるnodeについて
+        node = Nodes[node_index]
         node.insert(level-1) #１つ下のレベルにnodeを作成
 
 end1 = time.time()
 zdd_time = end1 - start
 
-trueend.parents.pop(0) #空白が格納されているので除く
+del Nodes['(-1, 0)'] #0終端にはparentsを格納していないので除く
+del Nodes[str((nodesum, 0))] #根にはparentsを格納していないので除く
+Nodes['(0, 0)'].parents.pop(0) #空白が格納されているので除く
 
-Nodes_Energy = {} #Nodes_Energy[node][エネルギー合計]=場合の数
-Nodes_Energy[trueend] = {} #初期化
-Nodes_Energy[trueend][0] = 1
+Nodes_Energy = {} #Nodes_Energy[node.index][エネルギー合計]=場合の数
+Nodes_Energy['(0, 0)'] = {} #初期化
+Nodes_Energy['(0, 0)'][0] = 1
 
 for level in range(0, nodesum): #0~nodesumまでの各レベルについて下からエネルギー計算
-    for node in levelset[level+1]: #次のレベルのノードのNodes_Energyを作成
-        Nodes_Energy[node] = {}
-    for node in levelset[level]: #そのレベルに存在するノードについて
-        for parent in node.parents: #各parentについて
-            for energysum in Nodes_Energy[node]: #各energysumについて
+    for node_index in levelset[level+1]: #次のレベルのノードのNodes_Energyを作成
+        Nodes_Energy[node_index] = {}
+    for node_index in levelset[level]: #そのレベルに存在するノードについて
+        for parent in Nodes[node_index].parents: #各parentについて
+            for energysum in Nodes_Energy[node_index]: #各energysumについて
                 new_energysum = energysum + energy_dic[parent[1]]
                 if new_energysum in Nodes_Energy[parent[0]]: #new_energysumがparentsに存在したら
-                    Nodes_Energy[parent[0]][new_energysum] += Nodes_Energy[node][energysum]
+                    Nodes_Energy[parent[0]][new_energysum] += Nodes_Energy[node_index][energysum]
                 else:
-                    Nodes_Energy[parent[0]][new_energysum] = Nodes_Energy[node][energysum]
-    for node in levelset[level]:
-        del Nodes_Energy[node]
+                    Nodes_Energy[parent[0]][new_energysum] = Nodes_Energy[node_index][energysum]
+    for node_index in levelset[level]:
+        del Nodes_Energy[node_index]
 
 dp_time = time.time() - end1
 
-znodesum = 0 #zddのノード総数
-for level in range(0, nodesum):
-    for node in levelset[level]: #そのレベルに存在するノードについて
-        znodesum += 1
-
 sum = 0
-f = open('newzddresult.txt', 'w')
+f = open('zddresult.txt', 'w')
 
-for energysum in Nodes_Energy[root]: #配置パターン総数を求める
-    sum += Nodes_Energy[root][energysum]
-
-elapsed_time = time.time() - start
+for energysum in Nodes_Energy[root.index]: #配置パターン総数を求める
+    f.write(str(energysum) + " " +str(Nodes_Energy[root.index][energysum]) + "\n")
+    sum += Nodes_Energy[root.index][energysum]
 
 f.write("input" + str(input) + "の配置総数は" + str(sum) + "\n")
-f.write("ノード総数は" + str(znodesum) + "\n")
+f.write("ノード総数は" + str(len(Nodes)) + "\n")
 # f.write("ループは" + str(Node.count) + "\n" + "ラベル生成は" + str(Node.count*(input+1)) + "\n")
 f.write("zdd_time {0}".format(zdd_time) + "[sec]" + "\n")
 f.write("dp_time {0}".format(dp_time) + "[sec]")
-
-for energysum in Nodes_Energy[root]: #配置パターン総数を求める
-    f.write(str(energysum) + " " +str(Nodes_Energy[root][energysum]) + "\n")
-
+f.write("dp時間計測方法変更")
 f.close()
 
 # G = Digraph(format='png') #Graphviz
 # G.attr('node', shape='circle')
 #
-# for node in Nodes:
+# for node_index in Nodes:
 #     G.node(node_index)
 #     temp_parents = Nodes[node_index].parents
 #     for parent in temp_parents:
