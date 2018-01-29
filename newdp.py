503cd5cc5a53e8ae5c5fcb2a39c7de2ee73247d3
Cats-Eye <ef_beee_i@yahoo.co.jp>
2018-01-29 15:44:42 +0900

    エネルギーとフロンティアを交換

--- newdp.py
+++ newdp.py
@@ -3,7 +3,7 @@ import time
 
 start = time.time()
 
-input = 3#考えたいgridの一辺の長さ
+input = 12#考えたいgridの一辺の長さ
 
 # 対象ノードの(左, 下)の状態からみて可能なパターン
 # -1は方向未処理、0は負方向、1は正方向
@@ -51,54 +51,58 @@ for node in range(nodesum, 0, -1): #各nodeについて
     place = (nodesum - node + 1) % input #左から数えたnode位置
     if place == 0: #右端は割り切れて0になるのでinputに書き換え
         place = input
-    for energysum in OldFronts:
-        for label in OldFronts[energysum]:
-            temp_label = label #次に渡すラベル準備
-            temp_label = temp_label & musk[0] #0(nodeの左)をマスク
-            temp_label = temp_label & musk[place] #place(nodeの下)をマスク
 
-            if node >= nodesum-input+1: #最下段の場合、place(nodeの下)は未処理(-1)
-                if place == 1: #行が１つ上になって左端の場合、0(nodeの左)は未処理(-1)
-                    patterns = pattern_dic[(-1,-1)]
-                else:
-                    patterns = pattern_dic[(label & 1,-1)]
+    for label in OldFronts:
+        temp_label = label #次に渡すラベル準備
+        temp_label = temp_label & musk[0] #0(nodeの左)をマスク
+        temp_label = temp_label & musk[place] #place(nodeの下)をマスク
+
+        if node >= nodesum-input+1: #最下段の場合、place(nodeの下)は未処理(-1)
+            if place == 1: #行が１つ上になって左端の場合、0(nodeの左)は未処理(-1)
+                patterns = pattern_dic[(-1,-1)]
             else:
-                if place == 1: #行が１つ上になって左端の場合、0(nodeの左)は未処理
-                    patterns = pattern_dic[(-1, (label >> place) & 1)]
-                else:
-                    patterns = pattern_dic[(label & 1, (label >> place) & 1)]
+                patterns = pattern_dic[(label & 1,-1)]
+        else:
+            if place == 1: #行が１つ上になって左端の場合、0(nodeの左)は未処理
+                patterns = pattern_dic[(-1, (label >> place) & 1)]
+            else:
+                patterns = pattern_dic[(label & 1, (label >> place) & 1)]
 
-            for i in patterns: #有りパターン
-                new_label = temp_label
-                if direction_dic[i][0] == 1: #ラベルを更新
-                    new_label += 1
-                if direction_dic[i][1] == 1:
-                    new_label += (1 << place)
+        for i in patterns: #有りパターン
+            new_label = temp_label
+            if direction_dic[i][0] == 1: #ラベルを更新
+                new_label += 1
+            if direction_dic[i][1] == 1:
+                new_label += (1 << place)
+
+            for energysum in OldFronts[label]:
                 new_energysum = energysum + energy_dic[i] #エネルギー合計を更新
 
-                if (new_energysum in NewFronts) and (new_label in NewFronts[new_energysum]): #同じ条件のものがあれば
-                    NewFronts[new_energysum][new_label] += OldFronts[energysum][label] #場合の数を追加
+                if (new_label in NewFronts) and (new_energysum in NewFronts[new_label]): #同じ条件のものがあれば
+                    NewFronts[new_label][new_energysum] += OldFronts[label][energysum] #場合の数を追加
                 else: #同じ条件のものがなかった場合
-                    NewFronts.setdefault(new_energysum, {}).setdefault(new_label, OldFronts[energysum][label]) #新しくfrontを作る
+                    NewFronts.setdefault(new_label, {}).setdefault(new_energysum, OldFronts[label][energysum]) #新しくfrontを作る
                     frontsum += 1
     OldFronts = NewFronts
     NewFronts = {}
 
 Energy = {}
 sum = 0
-for energysum in OldFronts:
-    Energy[energysum] = 0
-    for label in OldFronts[energysum]:
-        sum += OldFronts[energysum][label] #配置パターン総数を求める
-        Energy[energysum] += OldFronts[energysum][label] #エネルギー総計毎にまとめる
+for label in OldFronts:
+    for energysum in OldFronts[label]:
+        sum += OldFronts[label][energysum] #配置パターン総数を求める
+        if energysum in Energy:#同じ条件のものがあれば
+            Energy[energysum] += OldFronts[label][energysum] #場合の数を追加
+        else: #同じ条件のものがなかった場合
+            Energy[energysum] = OldFronts[label][energysum]#新しく作る
 
 elapsed_time = time.time() - start
-f = open('result.txt', 'w')
+f = open('newdpresult.txt', 'w')
 f.write("input" + str(input) + "の配置総数は" + str(sum) + "\n")
 f.write("elapsed_time:{0}".format(elapsed_time) + "\n")
 f.write("フロント総数" + str(frontsum) +"\n")
 
-# for k, v in sorted(Energy.items()):
-#     f.write(str(k) + " " + str(v) + "\n")
+for k, v in sorted(Energy.items()):
+    f.write(str(k) + " " + str(v) + "\n")
 
 f.close()
